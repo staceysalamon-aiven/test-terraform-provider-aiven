7b7e72ac61a266404675e914d1b3c585819e57f8
Stacey Salamon <stacey.salamon@aiven.io>
2025-03-07 12:38:40 +0100

    changes to test updating code blocks

--- examples/resources/aiven_organizational_unit/resource.tf
+++ examples/resources/aiven_organizational_unit/resource.tf
@@ -1,4 +1,4 @@
 resource "aiven_organizational_unit" "example_unit" {
   name      = "Example organizational unit"
-  parent_id = aiven_organization.main.id
+  parent_id = aiven_organization.main.org_id
 }
