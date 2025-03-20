55e61ff3abb1eb1cf9aba4b2112149485de5daa7
Stacey Salamon <stacey.salamon@aiven.io>
2025-03-20 12:34:44 +0100

    update org unit name again for modified workflow

--- examples/resources/aiven_organizational_unit/resource.tf
+++ examples/resources/aiven_organizational_unit/resource.tf
@@ -1,4 +1,4 @@
 resource "aiven_organizational_unit" "example_unit" {
-  name      = "Example organizational unit - changed name for testing"
+  name      = "Example organizational unit - changed name at 12:34"
   parent_id = aiven_organization.main.org_id
 }
