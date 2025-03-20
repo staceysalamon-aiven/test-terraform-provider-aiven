33f4f6480603d23548cb6d1847b01e99d4dc0e60
Stacey Salamon <stacey.salamon@aiven.io>
2025-03-20 11:35:47 +0100

    update org unit name again

--- examples/resources/aiven_organizational_unit/resource.tf
+++ examples/resources/aiven_organizational_unit/resource.tf
@@ -1,4 +1,4 @@
 resource "aiven_organizational_unit" "example_unit" {
-  name      = "Example organizational unit - changed"
+  name      = "Example organizational unit - changed name for testing"
   parent_id = aiven_organization.main.org_id
 }
