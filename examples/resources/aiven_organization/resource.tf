9f69f466fcb2edc417a839506265c49dacb7208b
Stacey Salamon <stacey.salamon@aiven.io>
2025-03-20 12:50:17 +0100

    1249 changes

--- examples/resources/aiven_organization/resource.tf
+++ examples/resources/aiven_organization/resource.tf
@@ -1,3 +1,3 @@
 resource "aiven_organization" "org" {
-  name = "Example organization"
+  name = "Example organization name changes for testing at 12:49"
 }
--- examples/resources/aiven_organizational_unit/resource.tf
+++ examples/resources/aiven_organizational_unit/resource.tf
@@ -1,4 +1,4 @@
 resource "aiven_organizational_unit" "example_unit" {
-  name      = "Example organizational unit - changed name at 12:43"
+  name      = "Example organizational unit - changed name at 12:49"
   parent_id = aiven_organization.main.org_id
 }
