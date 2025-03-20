823226b4d3cb248e4cad606afee534db6ab279b9
Stacey Salamon <stacey.salamon@aiven.io>
2025-03-20 12:43:58 +0100

    1243

--- examples/resources/aiven_organizational_unit/resource.tf
+++ examples/resources/aiven_organizational_unit/resource.tf
@@ -1,4 +1,4 @@
 resource "aiven_organizational_unit" "example_unit" {
-  name      = "Example organizational unit - changed name at 12:34"
+  name      = "Example organizational unit - changed name at 12:43"
   parent_id = aiven_organization.main.org_id
 }
